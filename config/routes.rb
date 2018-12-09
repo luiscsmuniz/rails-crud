48315dbdebd12183d30499f185a8e577eb752bbe
Luis Carlos de Souza <luisc.souzamuniz@gmail.com>
2018-12-09 14:17:28 -0200

    delete route coins

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   #get '/moedas', to: 'coins#index'
 
   root to: 'welcome#index'
+
+  delete 'coins/:id(.:format)', :to => 'coins#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
